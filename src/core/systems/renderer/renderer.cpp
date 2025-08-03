dfb949e999e48f079e08d0cbbe4123df31329f62
lolguy91 <retek1544@gmail.com>
2025-08-03 16:01:37 +0200

    I must state that I am fucking retarded

--- src/core/systems/renderer/renderer.cpp
+++ src/core/systems/renderer/renderer.cpp
@@ -184,8 +184,8 @@ void __render_material(Entity e, VAO* vao, GLuint index_count, sh_rect_t canvas)
             Mat4 translation = translate(Vec3((float[3]){t->position[0], t->position[1], 0}));
             Mat4 scalation = scale(Vec3((float[3]){t->scale[0], t->scale[1], 0}));
             t->model = rotation * scalation * translation;
+            t->changed = false;
         }
-        
         tvp *= t->model;
     }
 
